7546b89f9dc39e8492852f319cf08d8ad4c5b4b5
divya sri <divya.manusani9@gmail.com>
2020-09-25 09:16:28 +0530

    Pawns play

--- chess.js
+++ chess.js
@@ -1,5 +1,4 @@
-var table = document.createElement("table");
-table.setAttribute('border', '1');
+
 var chesscoinswhite = ['&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;', '&#9815;', '&#9816;', '&#9814;'];
 var whitepawn = '&#9817;';
 var blackpawn = '&#9823;';
@@ -8,54 +7,81 @@ var spanBlack = [];
 var spanWhite = [];
 var spanBlackPawns = [];
 var spanWhitePawns = [];
-var resetx, resety, colorreset;
-
 var tr = [];
-for (var i = 0; i < 8; i++) {
-    tr[i] = document.createElement('tr');
-    for (var j = 0; j < 8; j++) {
-        tr[i][j] = document.createElement('td');
-        tr[i][j].setAttribute('onclick', `moveTo(${i},${j})`);
-        if (i % 2 === j % 2) {
-            tr[i][j].className = 'white';
-        }
-        else {
-            tr[i][j].className = 'black';
-        }
 
-        if (i === 0) {
-            spanBlack[j] = document.createElement('span');
-            spanBlack[j].innerHTML = chesscoinsblack[j];
-            spanBlack[j].setAttribute('class', 'blackcoin');
-            tr[i][j].append(spanBlack[j]);
-        }
+var a = document.createElement('a');
+a.setAttribute('href', '#');
+a.setAttribute('onclick', 'startGame()');
+a.innerHTML = 'Start 2 player Chess Game';
 
-        if (i === 1) {
-            spanBlackPawns[j] = document.createElement('span');
-            spanBlackPawns[j].innerHTML = blackpawn;
-            spanBlackPawns[j].setAttribute('class', 'blackpawn');
-            spanBlackPawns[j].setAttribute('onclick', `onClickBlackPawn(${i},${j})`);
-            tr[i][j].append(spanBlackPawns[j]);
-        }
-        if (i === 7) {
-            spanWhite[j] = document.createElement('span');
-            spanWhite[j].innerHTML = chesscoinswhite[j];
-            spanWhite[j].setAttribute('class', 'whitecoin');
-            tr[i][j].append(spanWhite[j]);
 
+var a1 = document.createElement('a');
+a1.setAttribute('href', '#');
+a1.setAttribute('onclick', 'resetGame()');
+a1.innerHTML = 'Reset Game';
+document.body.append(a,a1);
+a1.style.display="none";
+
+function startGame() {
+
+   
+    var table = document.createElement("table");
+    table.setAttribute('border', '1');
+    table.setAttribute('id', 'chessboard');
+    for (var i = 0; i < 8; i++) {
+        tr[i] = document.createElement('tr');
+        for (var j = 0; j < 8; j++) {
+            tr[i][j] = document.createElement('td');
+            tr[i][j].setAttribute('onclick', `moveTo(${i},${j})`);
+            if (i % 2 === j % 2) {
+                tr[i][j].className = 'white';
+            }
+            else {
+                tr[i][j].className = 'black';
+            }
+
+            if (i === 0) {
+                spanBlack[j] = document.createElement('span');
+                spanBlack[j].innerHTML = chesscoinsblack[j];
+                spanBlack[j].setAttribute('class', 'blackcoin');
+                tr[i][j].append(spanBlack[j]);
+            }
+
+            if (i === 1) {
+                spanBlackPawns[j] = document.createElement('span');
+                spanBlackPawns[j].innerHTML = blackpawn;
+                spanBlackPawns[j].setAttribute('class', 'blackpawn');
+                spanBlackPawns[j].setAttribute('onclick', `onClickBlackPawn(${i},${j})`);
+                tr[i][j].append(spanBlackPawns[j]);
+            }
+            if (i === 7) {
+                spanWhite[j] = document.createElement('span');
+                spanWhite[j].innerHTML = chesscoinswhite[j];
+                spanWhite[j].setAttribute('class', 'whitecoin');
+                tr[i][j].append(spanWhite[j]);
+
+            }
+            if (i === 6) {
+                spanWhitePawns[j] = document.createElement('span');
+                spanWhitePawns[j].innerHTML = whitepawn;
+                spanWhitePawns[j].setAttribute('class', 'whitepawn');
+                spanWhitePawns[j].setAttribute('onclick', `onClickWhitePawn(${i},${j})`);
+                tr[i][j].append(spanWhitePawns[j]);
+            }
+            tr[i].append(tr[i][j]);
         }
-        if (i === 6) {
-            spanWhitePawns[j] = document.createElement('span');
-            spanWhitePawns[j].innerHTML = whitepawn;
-            spanWhitePawns[j].setAttribute('class', 'whitepawn');
-            spanWhitePawns[j].setAttribute('onclick', `onClickWhitePawn(${i},${j})`);
-            tr[i][j].append(spanWhitePawns[j]);
-        }
-        tr[i].append(tr[i][j]);
+        table.append(tr[i]);
     }
-    table.append(tr[i]);
+    document.body.append(a, table);
+    a1.style.display='block';
+    a.style.display='none';
 }
-document.body.append(table);
+function resetGame() {
+    
+    
+    document.getElementById('chessboard').remove();
+    startGame();
+}
 
 function onClickBlackPawn(x, y) {
     console.log('blackxy=', x, y);
@@ -85,7 +111,7 @@ function onClickWhitePawn(x, y) {
     repaint();
     tr[x][y].setAttribute('style', 'background-color:tomato;');
     if (!tr[x - 1][y].hasChildNodes()) {
-    tr[x - 1][y].setAttribute('style', 'background-color:yellow;');
+        tr[x - 1][y].setAttribute('style', 'background-color:yellow;');
     }
     if (x === 6) {
         tr[x - 2][y].setAttribute('style', 'background-color:yellow;');
@@ -149,6 +175,8 @@ function getFromXFromYValues() {
         }
     }
 }
+
+
 
 
 
